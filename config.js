d314ed78288660ccfd0ceca3b6dcc9b0f688c0cc
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-17 09:30:02 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
  * CONFIGURACIÓN APP REPORTES AGRUPADOS UTCD
  ******************************************************/
 const CONFIG = {
-  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbwLMZFsq2WDXHsYsWmqFpfjGpX50GZin70PihX0STlaQw0LYRm4hEme2sK1nvHM7feF/exec"
+  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbyCFVN-6skZVPOZ1RBvTxH4pLjk_RSUnED3A66X6slG74W98lvGwce8IEkzTq4V2Pzn/exec"
 };

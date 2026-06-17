f642eec8df461973c24949bd0c0420944097a45e
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-16 22:28:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * Cambia URL_APPS_SCRIPT después de implementar el Apps Script.
  ******************************************************/
 const CONFIG = {
-  URL_APPS_SCRIPT: 'https://script.google.com/macros/s/AKfycbyaPyABqEi1JhCa-AxsD7wkWrRpGTuZ4UM1Q4I2XCNuc-o89A3LYjrwcKZh9WUoH7Br/exec',
+  URL_APPS_SCRIPT: 'https://script.google.com/macros/s/AKfycbyTntIAfB-5i-hAzmkMEJ7uHeDDiG6LLYimLv_rmWQ7GafqfCOKX0m7QL07x6WdUp_2/exec',
   APP_NAME: 'REPORTES AGRUPADOS UTCD'
 };

476148f538bdeac32b78f041827e088c4757784a
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-17 10:21:38 -0600

    Update CONFIG to window.CONFIG for global access

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 /******************************************************
  * CONFIGURACIÓN APP REPORTES AGRUPADOS UTCD
  ******************************************************/
-const CONFIG = {
+
+window.CONFIG = {
   URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbzXEIO1Xx1b2S6-u_1A6kSLNm3SyGJQ3eI4SW-8k1fl3aI1hlWLE4C4u4zJtM0zup3g/exec"
 };
